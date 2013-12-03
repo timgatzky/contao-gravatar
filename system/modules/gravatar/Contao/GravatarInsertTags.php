cc703d982566b50c43aaa30b2e32b603414830cc
Tim Gatzky <info@tim-gatzky.de>
2013-12-03 15:04:56 +0100

    added more inserttags

--- system/modules/gravatar/Contao/GravatarInsertTags.php
+++ system/modules/gravatar/Contao/GravatarInsertTags.php
@@ -11,11 +11,26 @@
  * @link		http://contao.org
  * @license		http://www.gnu.org/licenses/lgpl-3.0.html LGPL
  */
- 
+
+/*
+
+Available inserttags:
+
+{{gravatar}}
+{{gravatar::member::ID-or-USERNAME}}
+{{gravatar::user::ID-or-USERNAME}}
+{{gravatar::EMAIL}}
+*/
+
 /**
  * Namespace
  */
 namespace Contao;
+
+/**
+ * Imports
+ */
+use \Contao\Gravatar as Gravatar;
 
 /**
  * Class file
@@ -36,41 +51,87 @@ class GravatarInsertTags extends \Controller
 		switch($arrElements[0])
 		{
 			case 'gravatar':
-				if(TL_MODE == 'FE')
+				if($arrElements[1])
 				{
-					if(!FE_USER_LOGGED_IN)
+					// check if an email is given
+					if(strpos($arrElements[1],'@'))
 					{
-						return false;
+						return Gravatar::get($arrElements[1]);
 					}
 					
-					$this->import('FrontendUser','User');
+					// check for member/user reference
+					switch($arrElements[1])
+					{
+						case 'member':
+							$objDatabase = \Database::getInstance();
+							
+							// fetch from id
+							if(is_numeric($arrElements[2]))
+							{
+								$objUser = $objDatabase->prepare("SELECT * FROM tl_member WHERE id=?")->limit(1)->execute($arrElements[2]);
+							}
+							// fetch from username
+							else if(is_string($arrElements[2]))
+							{
+								$objUser = $objDatabase->prepare("SELECT * FROM tl_member WHERE username=?")->limit(1)->execute($arrElements[2]);
+							}
+							else {return false;}
+							
+							if($objUser->numRows < 1)
+							{
+								return false;
+							}
+							
+							return Gravatar::get($objUser->email);
+							break;
+						case 'user':
+							$objDatabase = \Database::getInstance();
+							
+							// fetch from id
+							if(is_numeric($arrElements[2]))
+							{
+								$objUser = $objDatabase->prepare("SELECT * FROM tl_user WHERE id=?")->limit(1)->execute($arrElements[2]);
+							}
+							// fetch from username
+							else if(is_string($arrElements[2]))
+							{
+								$objUser = $objDatabase->prepare("SELECT * FROM tl_user WHERE username=?")->limit(1)->execute($arrElements[2]);
+							}
+							else {return false;}
+							
+							if($objUser->numRows < 1)
+							{
+								return false;
+							}
+							
+							return Gravatar::get($objUser->email);
+							break;
+						default: 
+							return false;
+							break;
+					}
 				}
+				
+				if(!FE_USER_LOGGED_IN)
+				{
+					return false;
+				}
+					
+				$this->import('FrontendUser','User');
 				
 				$email = $this->User->gravatarEmail ? $this->User->gravatarEmail : $this->User->email;
-				$grav_url = "http://www.gravatar.com/avatar/" . md5( strtolower( trim( $email ) ) );
-				$grav_attributes = array
-				(
-					's' => $GLOBALS['TL_CONFIG']['gravatarSize'] ? round($GLOBALS['TL_CONFIG']['gravatarSize']) : 80,
-					'd' => $GLOBALS['TL_CONFIG']['gravatarDefault'] ? $GLOBALS['TL_CONFIG']['gravatarDefault'] : '',
-					'r' => $GLOBALS['TL_CONFIG']['gravatarMaxRating'] ? $GLOBALS['TL_CONFIG']['gravatarMaxRating'] : 'x',
-				);
-				$grav_url .= '?';
-				foreach($grav_attributes as $k => $v)
-				{
-					if($v == 'gravatarlogo')
-					{
-						continue;
-					}
-					$grav_url .= $k.'='.$v.'&';
-				}
-				$grav_url = substr($grav_url, 0,-1);
 				
-				$img_attributes = 'width="'.$size.'px" height="'.$size.'px"' . ' title="'.$this->User->username.'"';
-				return sprintf('<img src="%s" %s>',$grav_url,$img_attributes);
+				return Gravatar::get($email);
 				break;
 			default:
 				return false;
 				break;
 		}
 	}
+	
+	
+	
+	
+	
+	
 }
